ce141791cb33431896d4b378481f9d489fe35702
Daniel Raudschus <draudschus.mmt-b2013@fh.salzburg.ac.at>
2015-03-20 23:20:09 +0100

    building up posts in js and html 	modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ var app = angular.module('flapperNews', []);
 app.controller('MainCtrl', [
     '$scope',
     function($scope) {
-        $scope.test = 'Hello World!'; // binds to the {{test}} in index.html
+
+        $scope.posts = [ // if wanted accessible in the template, bind it with $scope!
+            {title: 'post 1', upvotes: 5},
+            {title: 'post 2', upvotes: 2},
+            {title: 'post 3', upvotes: 15},
+            {title: 'post 4', upvotes: 9},
+            {title: 'post 5', upvotes: 4}
+        ];
     }])
